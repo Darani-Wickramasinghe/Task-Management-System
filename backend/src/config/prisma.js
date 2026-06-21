6a7919186a997688a0609169a05bbaf2c3a126b0
jeriel-jr <jsalomsan@gmail.com>
2026-06-21 14:28:52 +0530

    remove prisma config file

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,8 +1,13 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import * as prismaClientPkg from '@prisma/client';
+import * as adapterPgPkg from '@prisma/adapter-pg';
 import pg from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
+
+// This handles the module format differences between local dev and Render
+const PrismaClient = prismaClientPkg.PrismaClient || prismaClientPkg.default?.PrismaClient;
+const PrismaPg = adapterPgPkg.PrismaPg || adapterPgPkg.default?.PrismaPg;
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
