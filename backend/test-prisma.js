9685711b9a513c90eb781ee3b7d913e8431c9a7d
jeriel-jr <jsalomsan@gmail.com>
2026-06-23 15:01:11 +0530

    feat: add diagnostic script to test Prisma connectivity with Supabase database

--- backend/test-prisma.js
+++ backend/test-prisma.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const url = "postgresql://postgres.clqxzwvaqnhsvupzvykq:MySimplePassword123TMS@aws-0-ap-northeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true";
+const url = "postgresql://postgres.clqxzwvaqnhsvupzvykq:wrongpassword@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true";
 
 const prisma = new PrismaClient({
   datasources: {
